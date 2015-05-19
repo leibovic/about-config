972299fdfdae349681ae0f4831eb314ae9a72347
Margaret Leibovic <margaret.leibovic@gmail.com>
2015-05-18 17:47:18 -0700

    try converting add-on to use jpm

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var self = require('sdk/self');
+var { add } = require('about-what');
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
-
-exports.dummy = dummy;
+add({
+  what: 'foo',
+  url: self.data.url('config.html')
+});
